ea0a0f6ec8f54b6d4ce43680bce6fe03ff436673
Allan Souza <vejeta300@hotmail.com>
2023-04-15 14:44:35 -0300

    finalizado!

--- main.js
+++ main.js
@@ -36,9 +36,30 @@ function ready(){
         input.addEventListener("change", quantityChanged);
     }
     // adicionado os produtos no carrinho
-    
+    var addCart = document.getElementsByClassName('add-cart')
+    for (var i = 0; i < addCart.length; i++){
+        var button = addCart[i]
+        button.addEventListener("click", addCartClicked);
+    }
+//botão de compra funcional
+document
+.getElementsByClassName('btn-buy')[0]
+.addEventListener('click', buyButtonClicked);
 
 }
+//botão de compra
+function buyButtonClicked(){
+    alert('Seu pedido foi Finalizado!')
+    var cartContent = document.getElementsByClassName('cart-content')[0]
+    while(cartContent.hasChildNodes()){
+        cartContent.removeChild(cartContent.firstChild);
+    }
+    updateTotal()
+}
+
+
+
+
 
 function removeCartItem(event){
     var buttonClicked = event.target
@@ -55,8 +76,53 @@ function quantityChanged(event){
     updateTotal();
 }
 
+//adicionando no carrinho
+function addCartClicked(event){
+    var button = event.target;
+    var shopProducts = button.parentElement;
+    var title = shopProducts.getElementsByClassName('product-title')[0].innerText;
+    var price = shopProducts.getElementsByClassName('price')[0].innerText;
+    var productImg = shopProducts.getElementsByClassName('product-img')[0].src;
+
+    addProductToCart(title,price,productImg);
+    updateTotal();
+
+}
+function addProductToCart(title,price,productImg){
+    var cartShopBox = document.createElement("div");
+    cartShopBox.classList.add('cart-box');
+    var cartItems = document.getElementsByClassName("cart-content")[0];
+    var cartItemsNames = cartItems.getElementsByClassName("cart-product-title");
+    for (var i = 0; i <cartItemsNames.length; i++ ){
+        if (cartItemsNames[i].innerText == title) {
+            alert("Você ja adicionou esse produto no carrinho!");
+            return;
+        }
+    }
 
 
+var cartBoxContent = ` 
+                            <img src="${productImg}" alt="" class="cart-img">
+                            <div class="detail-box">
+                             <div class="cart-product-title">${title}</div>
+                            <div class="cart-price">${price}</div>
+                            <input type="number" value="1" class="cart-quantity">
+                            </div>  
+                            <!--Remover do carrinho-->
+                            <i class='bx bxs-trash-alt cart-remove'></i>`;
+
+cartShopBox.innerHTML = cartBoxContent;
+cartItems.append(cartShopBox);
+
+cartShopBox
+.getElementsByClassName('cart-remove')[0]
+.addEventListener('click', removeCartItem);
+
+cartShopBox
+.getElementsByClassName('cart-quantity')[0]
+.addEventListener('change', quantityChanged);
+
+}
 
 //subindo o total
 
@@ -71,7 +137,7 @@ function updateTotal(){
         var price = parseFloat(priceElement.innerText.replace("R$", ""));
         var quantity = quantityElement.value;
         total = total + price * quantity;
-
+    }
         //caso o preço tennha alguns centavos
         total = Math.round (total*100) / 100;
 
@@ -81,5 +147,5 @@ function updateTotal(){
 
 
 
-    }
+    
 }
